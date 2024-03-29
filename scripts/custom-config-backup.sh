ef88000cecc8289fa4021da0bb853ff8927770d5
monosense <eko.pur25@gmail.com>
2024-03-29 21:29:09 +0700

    chore: configure backup flash

--- scripts/custom-config-backup.sh
+++ scripts/custom-config-backup.sh
@@ -3,7 +3,7 @@
 # Description: Backup VyOS configuration to a USB device
 #
 dest="/media/usb-backup"
-disk="/dev/disk/by-id/usb-USB_SanDisk_3.2Gen1_0501a7b57d3aebec39c1d5c2222ed068372d5e7cd6d2c74e4c775e5ee97edac01d4c000000000000000000008e80aa130084171083558107cc2c2c67-0:0-part1"
+disk="/dev/disk/by-id/usb-SanDisk__Cruzer_Fit_02000904062321085848-0:0-part1"
 user="vyos"
 
 # Mount formatted usb drive
